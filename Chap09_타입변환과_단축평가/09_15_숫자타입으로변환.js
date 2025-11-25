1095675c663b45d314cdb6ced300fe7d97718d4e
hyunjinkim <tcmhdn77@naver.com>
2025-11-25 23:59:18 +0900

    251125

--- Chap09_타입변환과_단축평가/09_15_숫자타입으로변환.js
+++ Chap09_타입변환과_단축평가/09_15_숫자타입으로변환.js
@@ -11,39 +11,39 @@
 
 // 1. Number 생성자 함수를 new 연산자 없이 호출
 // 문자열
-console.log('1: ' + (Number('0')));
-console.log('2: ' + (Number('-1')));
-console.log('3: ' + (Number('10.53')));
+console.log(Number('0'));  // 0
+console.log(Number('-1'));  // -1
+console.log(Number('10.53'));  // 10.53
 
 // 불리언
-console.log('4: ' + (Number(true)));
-console.log('5: ' + (Number(false)));
+console.log(Number(true));  // 1
+console.log(Number(false));  // 0
 
 
 // 2. parseInt, parseFloat 함수를 사용하는 방법 (문자열만 숫자 타입으로 변환 가능)
 // 문자열
-console.log('6: ' + (parseInt('0')));
-console.log('7: ' + (parseInt('-1')));
-console.log('8: ' + (parseInt('10.53')));
+console.log(parseInt('0'));  // 0
+console.log(parseInt('-1'));  // -1
+console.log(parseInt('10.53'));  // 10
 
 
 // 3. + 단항 산술 연산자를 이용
 // 문자열
-console.log('9: ' + (+'0'));
-console.log('10: ' + (+'-1'));
-console.log('11: ' + (+'10.53'));
+console.log(+'0');  // 0
+console.log(+'-1');  // -1
+console.log(+'10.53');  // 10.53
 
 // 불리언
-console.log('12: ' + (+true));
-console.log('13: ' + (+false));
+console.log(+true);  // 1
+console.log(+false);  // 0
 
 
 // 4. * 산술 연산자를 이용
 // 문자열
-console.log('14: ' + ('0' * 1));
-console.log('15: ' + ('-1' * 1));
-console.log('16: ' + ('10.53' * 1));
+console.log('0' * 1);  // 0
+console.log('-1' * 1);  // -1
+console.log('10.53' * 1);  // 10.53
 
 // 불리언
-console.log('16: ' + (true * 1));
-console.log('16: ' + (false * 1));
+console.log(true * 1);  // 1
+console.log(false * 1);  // 0
